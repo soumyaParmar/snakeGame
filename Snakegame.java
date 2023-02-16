d0bfbfde53c9268d7de8f611223e784ccba0887d
soumyaParmar <125345243+soumyaParmar@users.noreply.github.com>
2023-02-16 15:23:29 +0530

    Create Snakegame.java

---
+++ Snakegame.java
@@ -0,0 +1,12 @@
+package com.mycompany.snakegame;
+
+/**
+ *
+ * @author soumy
+ */
+public class Snakegame {
+
+    public static void main(String[] args) {
+        GameFrame game = new GameFrame();
+    }
+}
